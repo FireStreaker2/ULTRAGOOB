2bb99356691bcc9d4addddfcbcc2fa592708f6bb
FireStreaker2 <103970465+FireStreaker2@users.noreply.github.com>
2023-05-01 17:50:51 -0700

    starting point for project

---
+++ obj/Debug/netstandard2.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace ULTRAGOOB
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "ULTRAGOOB";
+        public const string PLUGIN_NAME = "ULTRAGOOB";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
